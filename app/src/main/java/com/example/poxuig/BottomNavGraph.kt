232779beade21b1cb95652c292d34103d69ee186
Bezmen Evgeny <flashlight1011@yahoo.com>
2022-02-28 11:56:23 +0300

    add base bottom bar

--- app/src/main/java/com/example/poxuig/BottomNavGraph.kt
+++ app/src/main/java/com/example/poxuig/BottomNavGraph.kt
@@ -1,2 +1,30 @@
 package com.example.poxuig
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.poxuig.screens.CalendarScreen
+import com.example.poxuig.screens.HomeScreen
+import com.example.poxuig.screens.ProfileScreen
+
+@Composable
+fun BottomNavGraph(navController: NavHostController){
+    NavHost(
+        navController = navController,
+        startDestination = BottomBarScreen.Home.route){
+        composable(route = BottomBarScreen.Home.route){
+            HomeScreen()
+        }
+        composable(route = BottomBarScreen.Calendar.route){
+            CalendarScreen()
+        }
+        composable(route = BottomBarScreen.Profile.route){
+            ProfileScreen()
+        }
+    }
+
+}
+
+
